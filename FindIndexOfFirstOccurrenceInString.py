327034b90fbc37603bc03708a7f908e31be9dedd
Yahia Walid <yahiawalidzaky@gmail.com>
2023-05-11 22:52:10 +0300

    Create FindIndexOfFirstOccurrenceInString.py

---
+++ FindIndexOfFirstOccurrenceInString.py
@@ -0,0 +1,23 @@
+#28. Find the Index of the First Occurrence in a String
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        n_haystack = len(haystack)
+        n_needle = len(needle)
+
+
+        for i in range(n_haystack):
+            if (i+n_needle) < n_haystack :
+                if haystack[i:n_needle] == needle:
+                    return i
+
+        return -1
+
+
+s = Solution()
+
+print(s.strStr("sadbutsad","sad"))
